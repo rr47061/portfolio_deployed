a60ca3235fd672e4a48e836729b889b4eca8fb68
rr47061 <ranjan.rohit64@gmail.com>
2019-04-24 17:35:53 +0530

    build

---
+++ build/precache-manifest.efd6b0b5197054adc6949df4e1c6815f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "59c49ab772459b0a0846",
+    "url": "/static/css/main.3079521a.chunk.css"
+  },
+  {
+    "revision": "59c49ab772459b0a0846",
+    "url": "/static/js/main.59c49ab7.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "81cb3d662f2a5d0f03d0",
+    "url": "/static/css/2.6fd7dba7.chunk.css"
+  },
+  {
+    "revision": "81cb3d662f2a5d0f03d0",
+    "url": "/static/js/2.81cb3d66.chunk.js"
+  },
+  {
+    "revision": "fdcffb094d94e4f3ed84a791cc4c64f0",
+    "url": "/index.html"
+  }
+];
